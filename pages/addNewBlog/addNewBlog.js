af5d3bd81029db2de8fb459bace5f74da5189aba
TheJesterEnd <kakhniashvililuka@gmail.com>
2024-02-02 13:03:35 +0400

    feat: save blog information in local storage

--- pages/addNewBlog/addNewBlog.js
+++ pages/addNewBlog/addNewBlog.js
@@ -4,21 +4,142 @@ const inputFile = document.querySelector("#input-file");
 const imgUploadContainer = document.querySelector(".img-upload-container");
 const fileNameP = document.querySelector("#file-name_p");
 const remove = document.querySelector("#remove");
-//-----------------------------------------------------------This is where the logic of drag and drop begins
+const titleInput = document.querySelector("#title-input");
+const titleInputPara = document.querySelector("#title-input_para");
+const descriptionInput = document.querySelector("#description-input");
+const descriptionInputPara = document.querySelector("#description-input_para");
+const dateInput = document.querySelector("#date-input");
+const dropDownMenuContent = document.querySelector(".drop-down_menu");
+const categoryDiv = document.querySelector(".category-div");
+const h4 = document.querySelector("h4");
+
 let imgUrl;
+let fileName;
+let storedCategoriesData;
+// Load data from localStorage when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  const storedImgUrl = localStorage.getItem("imgUrl");
+  const storedFileName = localStorage.getItem("fileName");
+
+  if (storedImgUrl && storedFileName) {
+    imgUploadContainer.style.display = "flex";
+    fileBox.style.display = "none";
+    fileNameP.textContent = storedFileName;
+    imgUrl = storedImgUrl;
+  } else {
+    fileBox.style.display = "flex";
+    imgUploadContainer.style.display = "none";
+  }
+  const storedAuthorValue = localStorage.getItem("authorValue");
+  if (storedAuthorValue) {
+    authorInput.defaultValue = storedAuthorValue;
+    authorValidation();
+    authorValidationFinal();
+  }
+  const storedTitleValue = localStorage.getItem("titleValue");
+  if (storedTitleValue) {
+    titleInput.defaultValue = storedTitleValue;
+    titleValidation(titleInput, titleInputPara);
+    titleValidationFinal(titleInput, titleInputPara);
+  }
+  const storedDescriptionValue = localStorage.getItem("descriptionValue");
+  if (storedDescriptionValue) {
+    descriptionInput.defaultValue = storedDescriptionValue;
+    titleValidation(descriptionInput, descriptionInputPara);
+    titleValidationFinal(descriptionInput, descriptionInputPara);
+  }
+  const storedDateValue = localStorage.getItem("dateValue");
+  if (storedAuthorValue) {
+    dateInput.defaultValue = storedDateValue;
+    dateInputValidation();
+  }
+  const storedEmailValue = localStorage.getItem("emailValue");
+  if (storedEmailValue) {
+    emailInput.defaultValue = storedEmailValue;
+    emailValidation();
+  }
+  storedCategoriesData = JSON.parse(localStorage.getItem("categoriesData"));
+  if (storedCategoriesData && storedCategoriesData.length > 0) {
+    for (let i = 0; i < storedCategoriesData.length; i++) {
+      const button = document.createElement("div");
+      button.className = "button-container";
+      button.style.color = "white";
+      button.innerHTML += `
+          <div class="button-background" style="background:${storedCategoriesData[i].background_color};opacity:1"></div>
+          <button class="my-button category-button">${storedCategoriesData[i].title}</button>
+        `;
+      const closeButtonSpan = document.createElement("span");
+      closeButtonSpan.textContent = "X";
+      closeButtonSpan.className = "close-button";
+      button.lastElementChild.appendChild(closeButtonSpan);
+      categoryDiv.appendChild(button);
+      button.lastElementChild.addEventListener("click", (event) => {
+        event.preventDefault();
+      });
+      if (categoryDiv.children.length > 2) {
+        h4.style.display = "none";
+      }
+      closeButtonSpan.addEventListener("click", (event) => {
+        for (let j = 0; j < dropDownMenuContent.children.length; j++) {
+          console.log(
+            event.target.previousSibling.textContent.trim(),
+            dropDownMenuContent.children[j].textContent.trim()
+          );
+
+          if (
+            event.target.previousSibling.textContent.trim() ===
+            dropDownMenuContent.children[j].textContent.trim()
+          ) {
+            dropDownMenuContent.children[j].firstElementChild.style.opacity =
+              "0.08";
+            dropDownMenuContent.children[j].style.color =
+              storedCategoriesData[i].text_color;
+          }
+        }
+        categoryDiv.removeChild(button);
+        console.log(typeof event.target.previousElementSibling);
+        const indexToRemove = storedCategoriesData.findIndex((item) => {
+          console.log(item.title, event.target.previousSibling);
+          return item.title === event.target.previousSibling.textContent;
+        });
+        console.log(indexToRemove);
+        if (indexToRemove !== -1) {
+          storedCategoriesData.splice(indexToRemove, 1);
+          localStorage.setItem(
+            "categoriesData",
+            JSON.stringify(storedCategoriesData)
+          );
+        }
+        if (categoryDiv.children.length === 2) {
+          h4.style.display = "block";
+        }
+      });
+    }
+  }
+});
+
 inputFile.addEventListener("change", (event) => {
-  fileNameP.textContent = event.target.files[0].name;
-  imgUrl = URL.createObjectURL(event.target.files[0]);
-  dropFile();
+  const selectedFiles = event.target.files;
+  if (selectedFiles.length > 0) {
+    fileName = selectedFiles[0].name;
+    fileNameP.textContent = fileName;
+    imgUrl = URL.createObjectURL(selectedFiles[0]);
+    dropFile();
+  }
 });
 
 ["drag", "dragenter", "dragover", "drop"].forEach((eventName) => {
   dropArea.addEventListener(eventName, (event) => event.preventDefault());
 });
+
 dropArea.addEventListener("drop", (event) => {
-  fileNameP.textContent = event.dataTransfer.files[0].name;
-  imgUrl = URL.createObjectURL(event.dataTransfer.files[0]);
-  dropFile();
+  const droppedFiles = event.dataTransfer.files;
+  if (droppedFiles.length > 0) {
+    fileName = droppedFiles[0].name;
+    fileNameP.textContent = fileName;
+    imgUrl = URL.createObjectURL(droppedFiles[0]);
+    dropFile();
+  }
 });
 
 remove.addEventListener("click", removeFile);
@@ -27,20 +148,26 @@ function dropFile() {
   fileBox.style.display = "none";
   imgUploadContainer.style.display = "flex";
   localStorage.setItem("imgUrl", imgUrl);
+  localStorage.setItem("fileName", fileName);
 }
 
 function removeFile() {
   imgUploadContainer.style.display = "none";
   fileBox.style.display = "flex";
   localStorage.removeItem("imgUrl");
+  localStorage.removeItem("fileName");
 }
+
 //-------------------------------------------------This is where the validation of the author's input begins
 const authorInput = document.querySelector("#author-input");
 const fourSymbol = document.querySelector("#four-symbol");
 const twoWord = document.querySelector("#two-word");
 const georgian = document.querySelector("#georgian");
 
-authorInput.addEventListener("input", authorValidation);
+authorInput.addEventListener("input", () => {
+  authorValidation();
+  localStorage.setItem("authorValue", authorInput.value);
+});
 authorInput.addEventListener("change", authorValidationFinal);
 
 const georgianRegex = /[\u10A0-\u10EA\u10FC\u10EE\u10EB]/;
@@ -100,13 +227,13 @@ function authorValidationFinal() {
     }
   }
 }
-//----------------------------------------------------------------This is where title validation begins
-const titleInput = document.querySelector("#title-input");
-const titleInputPara = document.querySelector("#title-input_para");
+//----------------------------------------------------------------This is where title validation begins--------------------------------------
+
 let titleValue;
-titleInput.addEventListener("input", () =>
-  titleValidation(titleInput, titleInputPara)
-);
+titleInput.addEventListener("input", () => {
+  titleValidation(titleInput, titleInputPara);
+  localStorage.setItem("titleValue", titleInput.value);
+});
 titleInput.addEventListener("change", () => {
   titleValidationFinal(titleInput, titleInputPara);
   titleValue = titleInput.value.trim();
@@ -129,18 +256,37 @@ function titleValidationFinal(input, inputPara) {
     input.style.border = "1px solid #EA1919";
   }
 }
-//-----------------------------------------------------------Here I just added an event listener to the description input
-const descriptionInput = document.querySelector("#description-input");
-const descriptionInputPara = document.querySelector("#description-input_para");
+//-----------------------------------------------------------Here I just added an event listener to the description input-------------------
 let descriptionValue;
-descriptionInput.addEventListener("input", () =>
-  titleValidation(descriptionInput, descriptionInputPara)
-);
+descriptionInput.addEventListener("input", () => {
+  titleValidation(descriptionInput, descriptionInputPara);
+  localStorage.setItem("descriptionValue", descriptionInput.value);
+});
 descriptionInput.addEventListener("change", () => {
   titleValidationFinal(descriptionInput, descriptionInputPara);
   descriptionValue = descriptionInput.value.trim();
 });
+//----------------------------------------------------------------This is where email validation begins--------------------------------------
 
+const emailInput = document.querySelector("#email-input");
+const emailInputPara = document.querySelector("#email-span");
+const emailPattern = /^[\w.-]+@redberry\.ge$/;
+let emailValue;
+emailInput.addEventListener("change", () => {
+  emailValidation();
+  localStorage.setItem("emailValue", emailValue);
+});
+function emailValidation() {
+  if (emailPattern.test(emailInput.value)) {
+    emailValue = emailInput.value;
+    emailInput.style.border = "1px solid #14D81C";
+    emailInputPara.style.display = "none";
+  } else {
+    emailInputPara.style.display = "flex";
+    emailInput.style.border = "1px solid #EA1919";
+  }
+}
+//-----------------------
 const arrowIcon = document.querySelector("#arrow-icon");
 const dropDown = document.querySelector(".drop-down_menu");
 arrowIcon.addEventListener("click", dropDownMenu);
@@ -157,25 +303,7 @@ function dropDownMenu() {
     isClicked = false;
   }
 }
-const emailInput = document.querySelector("#email-input");
-const emailInputPara = document.querySelector("#email-span");
-const emailPattern = /^[\w.-]+@redberry\.ge$/;
-let emailValue;
-emailInput.addEventListener("change", emailValidation);
-function emailValidation() {
-  if (emailPattern.test(emailInput.value)) {
-    emailValue = emailInput.value;
-    emailInput.style.border = "1px solid #14D81C";
-    emailInputPara.style.display = "none";
-  } else {
-    emailInputPara.style.display = "flex";
-    emailInput.style.border = "1px solid #EA1919";
-  }
-}
-//-----------------------
-const dropDownMenuContent = document.querySelector(".drop-down_menu");
-const categoryDiv = document.querySelector(".category-div");
-const h4 = document.querySelector("h4");
+
 const categoriesData = [];
 async function getCategories() {
   try {
@@ -193,50 +321,63 @@ async function getCategories() {
           <button class="my-button category-button">${data[i].title}</button>
         `;
       dropDownMenuContent.appendChild(button);
-      let clicked = false;
 
-      button.addEventListener("click", (event) => {
-        event.preventDefault();
+      if (storedCategoriesData.find((obj) => obj.title === data[i].title)) {
         button.firstElementChild.style.opacity = "1";
         button.style.color = "white";
-        h4.style.display = "none";
+      } else {
+        let clicked = false;
 
-        const duplicateButton = event.target.parentElement.cloneNode(true);
-        // duplicateButton.className = "duplicate-button";
-        // Create the X icon
-        const closeButton = document.createElement("span");
-        closeButton.textContent = "X";
-        closeButton.className = "close-button";
+        button.addEventListener("click", (event) => {
+          event.preventDefault();
+          button.firstElementChild.style.opacity = "1";
+          button.style.color = "white";
+          h4.style.display = "none";
 
-        // Append the "X" to the cloned button
-        duplicateButton.lastElementChild.appendChild(closeButton);
+          const duplicateButton = event.target.parentElement.cloneNode(true);
+          // duplicateButton.className = "duplicate-button";
+          // Create the X icon
+          const closeButton = document.createElement("span");
+          closeButton.textContent = "X";
+          closeButton.className = "close-button";
 
-        // Append the cloned button to the categoryDiv
-        if (!clicked) {
-          categoryDiv.appendChild(duplicateButton);
-          categoriesData.push(data[i]);
-          console.log(categoriesData);
-        }
-        categoryDiv.addEventListener("click", (event) =>
-          event.preventDefault()
-        );
-        clicked = true;
-        duplicateButton.removeEventListener("click", getCategories);
+          // Append the "X" to the cloned button
+          duplicateButton.lastElementChild.appendChild(closeButton);
 
-        // Add click event listener to the X icon for removal
-        closeButton.addEventListener("click", () => {
-          button.firstElementChild.style.opacity = "0.08";
-          button.style.color = data[i].text_color;
-          clicked = false;
-          categoryDiv.removeChild(duplicateButton);
-          if (categoriesData.indexOf(data[i]) !== -1) {
-            categoriesData.splice(categoriesData.indexOf(data[i]), 1);
+          // Append the cloned button to the categoryDiv
+          if (!clicked) {
+            categoryDiv.appendChild(duplicateButton);
+            categoriesData.push(data[i]);
+            localStorage.setItem(
+              "categoriesData",
+              JSON.stringify(categoriesData)
+            );
           }
-          if (categoryDiv.children.length === 2) {
-            h4.style.display = "block";
-          }
+          categoryDiv.addEventListener("click", (event) =>
+            event.preventDefault()
+          );
+          clicked = true;
+          duplicateButton.removeEventListener("click", getCategories);
+
+          // Add click event listener to the X icon for removal
+          closeButton.addEventListener("click", () => {
+            button.firstElementChild.style.opacity = "0.08";
+            button.style.color = data[i].text_color;
+            clicked = false;
+            categoryDiv.removeChild(duplicateButton);
+            if (categoriesData.indexOf(data[i]) !== -1) {
+              categoriesData.splice(categoriesData.indexOf(data[i]), 1);
+              localStorage.setItem(
+                "categoriesData",
+                JSON.stringify(categoriesData)
+              );
+            }
+            if (categoryDiv.children.length === 2) {
+              h4.style.display = "block";
+            }
+          });
         });
-      });
+      }
     }
   } catch (error) {
     console.log(error.message);
@@ -244,12 +385,15 @@ async function getCategories() {
 }
 
 getCategories();
-const dateInput = document.querySelector("#date-input");
 let dateValue;
 dateInput.addEventListener("change", () => {
   dateValue = dateInput.value;
+  dateInputValidation();
+  localStorage.setItem("dateValue", dateValue);
 });
-
+function dateInputValidation() {
+  dateInput.style.border = "1px solid #14D81C";
+}
 //authorValue
 // emailValue;
 // localStorage.getItem("imgUrl")
