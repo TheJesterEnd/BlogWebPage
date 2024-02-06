338685a6864a60f3243e1d4c6719a6e70fd09865
TheJesterEnd <kakhniashvililuka@gmail.com>
2024-02-06 23:53:41 +0400

    feat: move for loop into loopPost function, make post filter functionallity

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cardBox = document.querySelector(".card-box");
 const buttons = document.querySelector(".buttons");
+let postData;
 let selectedCategories = [];
 async function fetchCategories() {
   try {
@@ -33,7 +34,21 @@ async function fetchCategories() {
           selectedCategories.splice(index, 1);
         }
       }
-      console.log(selectedCategories);
+
+      let filtered = postData.filter((card) => {
+        let some = selectedCategories.some((item) => {
+          let rame = card.categories.map((element) => {
+            return element.title;
+          });
+          return rame.includes(item.title);
+        });
+        return some;
+      });
+      cardBox.innerHTML = "";
+      if (filtered.length === 0) {
+        loopPosts(postData);
+      }
+      loopPosts(filtered);
     });
   } catch (error) {
     console.log(error.message);
@@ -47,38 +62,41 @@ async function fetchPosts() {
       "https://george.pythonanywhere.com/api/blogs/"
     );
     if (!response.ok) throw new Error("failed to fetch data");
-    const data = await response.json();
-    for (let i = 0; i < data.length; i++) {
-      cardBox.innerHTML += `
-        <div class="card">
-        <img
-          class="mobile"
-          src="${data[i].image}"
-          alt="mobile"
-        />
-        <p class="name">${data[i].author}</p>
-        <p class="date">${data[i].publish_date}</p>
-        <h1>${data[i].title}</h1>
-        <div class="card-buttons">
-          ${data[i].categories
-            .map((category) => {
-              return `<div class="button-container" style="color:${category.text_color}">
-              <div class="button-background" style="background:${category.background_color}"></div>
-              <button class="common-button my-button category-button">${category.title}</button>
-             </div>
-            `;
-            })
-            .join("")}
-        </div>
-        <h2 class="text">${data[i].description.slice(0, 100) + "..."}</h2>
-        <div class="last-part">
-          <button class="full-text">სრულად ნახვა</button>
-          <img class="arrow" src="./images/Arrow.png" alt="arrow" />
-        </div>
-      </div>`;
-    }
+    postData = await response.json();
+    loopPosts(postData);
   } catch (error) {
     // console.log(error.message);
   }
 }
 fetchPosts();
+function loopPosts(data) {
+  for (let i = 0; i < data.length; i++) {
+    cardBox.innerHTML += `
+      <div class="card">
+      <img
+        class="mobile"
+        src="${data[i].image}"
+        alt="mobile"
+      />
+      <p class="name">${data[i].author}</p>
+      <p class="date">${data[i].publish_date}</p>
+      <h1>${data[i].title}</h1>
+      <div class="card-buttons">
+        ${data[i].categories
+          .map((category) => {
+            return `<div class="button-container" style="color:${category.text_color}">
+            <div class="button-background" style="background:${category.background_color}"></div>
+            <button class="common-button my-button category-button">${category.title}</button>
+           </div>
+          `;
+          })
+          .join("")}
+      </div>
+      <h2 class="text">${data[i].description.slice(0, 100) + "..."}</h2>
+      <div class="last-part">
+        <button class="full-text">სრულად ნახვა</button>
+        <img class="arrow" src="./images/Arrow.png" alt="arrow" />
+      </div>
+    </div>`;
+  }
+}
